9a7db631519d18d05a149f1bf0011ac5ef155a8f
AndrewPraveenN <andrew@wearecuriousminds.com>
2024-03-10 15:23:25 +0530

    interview

--- app/Models/Medication.php
+++ app/Models/Medication.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Medication extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'name',
+        'description',
+        'stock',
+    ];
 }
